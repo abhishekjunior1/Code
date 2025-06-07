506f839f2e4128b551f4afa401e785722dc1b498
Abhishek Kumar <abhishekjunior1@gmail.com>
2025-06-08 02:29:01 +0530

    added Binary Search folder

---
+++ Advance DSA/Binary Search/basic.java
@@ -0,0 +1,30 @@
+package Advance DSA.Binary Search;
+
+public class basic {
+    public static void main(String[] args) {
+        // Example usage of binary search
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 5;
+        int result = binarySearch(arr, target);
+        System.out.println("Index of " + target + ": " + result);
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Target found
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+        return -1; // Target not found
+    }
+    
+}
